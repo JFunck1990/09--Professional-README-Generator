448495a5a9acf323fd130e45197ab6223e3b1ef8
Jack Funck <jackfunck21@gmail.com>
2021-02-04 16:58:44 -0500

    got all the tc links to work 'I hope'

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ let readMeGen = `
 
 1. [ Description. ](#desc)
 2. [How to install.](#install)
+3. [Usage.](#usage)
+4. [License.](#license)
+5. [Contributing.](#con)
+6. [Tests.](#tests)
+7. [Screen Shots.](#ss)
+8. [Questions.](#questions)
 
 <a name="desc"></a>
 ## Description 
@@ -21,33 +27,32 @@ ${response.description}
 ## How to install ${response.title}
 
 ${response.install}
-
+<a name="usage"></a>
 ##  Usage
 
 ${response.usage}
-
+<a name="license"></a>
 ## License
 
 ![GitHub license](https://img.shields.io/badge/license-${response.license}-blue.svg)
 
 
-
+<a name="con"></a>
 ## Contributing
 
 ${response.contribution}
-
+<a name="tests"></a>
 ## Tests
 
 ${response.test}
 
-## Video
 
 
-
+<a name="ss"></a>
 ## Screen Shots
 
 
-
+<a name="questions"><a>
 ## Questions
 > Email:${response.emil}
 > Github:${response.githubName}
